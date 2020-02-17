a9c92b57f1f06c0c3022f6adaafdebf35adf0ec7
udayanbirajdar <udayanbirajdar@gmail.com>
2020-02-17 15:40:23 -0800

    Remove classes

--- src/space_quiz/def/ABC.java
+++
@@ -1,8 +0,0 @@
-package space_quiz.def;
-
-public class ABC {
-	public static void main(String args[]){
-		System.out.println("asdsa");
-		System.out.println("Clifff Sameuls uthlelaaaa ahe");
-	}
-}
